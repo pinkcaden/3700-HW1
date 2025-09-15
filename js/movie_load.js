05e55fc9c04f83c24c3cf300a5fdf1de40af128e
Caden Pink <cadenpink@gmail.com>
2025-09-14 21:17:01 -0500

    09142025

--- js/movie_load.js
+++ js/movie_load.js
@@ -1,10 +1,21 @@
 
 
 
+const MOVIES_URL = "https://localhost:3000/movies"
+const MOCK_MOVIES_URL = "./data/movies.json";
+
+const movieObjectList = [];
 
 
-function moviePopulate(movieList) {
 
+async function moviePopulate(movieList) {
+
+    const response = await fetch(MOVIES_URL);
+    const movies = await response.json();
+
+}
+
+function getMoviesList(){
 
 
 }
--- js/movie_thumbnails.js
+++ js/movie_thumbnails.js
@@ -1,6 +1,33 @@
-
-
-
-function genThumbnails(){
-    return ``
+function genThumbnail(movieObject) {
+    console.log(movieObject);
+    return `
+    <div class="card">
+        <div class="card-body border-1">
+            <div class = "card-title">${movieObject["title"]} (${movieObject["year"]})</div>
+            <div class = "card-text">
+                ${movieObject["description"]}
+                </br><b>Box Office: </b> ${movieObject["boxOffice"]}
+            </div>
+            <a href = "https://google.com" class = "button">View Details</a>
+        </div>
+    </div>
+    
+    `
 }
+
+
+
+function genThumbnails(moviesList) {
+    console.log(moviesList);
+
+    let thumbnailList = ``
+    moviesList.forEach((movieObject) => {
+        alert('hi')
+
+        console.log(movieObject);
+            thumbnailList = thumbnailList + genThumbnail(movieObject);
+    })
+    return thumbnailList;
+}
+
+
